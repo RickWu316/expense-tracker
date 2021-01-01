13cfee23c78b1c6342db5434e013618b6c005571
Rick Wu <rickwu316@gmail.com>
2021-01-01 23:37:01 +0800

    feat: add close connet to mongoose

--- app.js
+++ app.js
@@ -36,18 +36,19 @@ Handlebars.registerHelper('ifEquals', function (arg1, arg2, options) {
     return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
 });
 
+require('./config/mongoose')
 
-mongoose.connect('mongodb://localhost/Records', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-    console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-    console.log('mongodb connected!')
-})
+// mongoose.connect('mongodb://localhost/Records', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+// // 取得資料庫連線狀態
+// const db = mongoose.connection
+// // 連線異常
+// db.on('error', () => {
+//     console.log('mongodb error!')
+// })
+// // 連線成功
+// db.once('open', () => {
+//     console.log('mongodb connected!')
+// })
 
 
 // // 將 request 導入路由器
--- models/seeds/recordSeeds.js
+++ models/seeds/recordSeeds.js
@@ -1,28 +1,27 @@
 
+const db = require('../../config/mongoose')
 const records = require('../records') // 載入 todo model
 // const categoryTransfer = require('../../category')
-
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/Records', { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-
-db.on('error', () => {
-    console.log('mongodb error!')
-})
 
 const category = ['home', 'shuttle', 'fun', 'food', 'other']
 
 db.once('open', () => {
-    console.log('mongodb connected!')
+    let record = []
     for (let i = 0; i < 10; i++) {
-        records.create({
-            name: 'name-' + i,
-            category: category[Math.floor(Math.random() * 4)],
-            date: `2020-12-${Math.floor(Math.random() * 30)}`,
-            amount: Math.floor(Math.random() * 1000)
-        })
+        record.push(
+            {
+                name: 'name-' + i,
+                category: category[Math.floor(Math.random() * 4)],
+                date: `2020-12-${Math.floor(Math.random() * 30)}`,
+                amount: Math.floor(Math.random() * 1000)
+            })
     }
-    console.log('done')
+    records.create(record)
+        .then(() => {
+            console.log('done')
+            return db.close()
+        }
+        )
 
 })
 
