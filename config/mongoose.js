29e34153386bd1172e76df2106aaf4b4e5158762
Rick Wu <rickwu316@gmail.com>
2021-01-03 03:02:49 +0800

    chore: heroku init

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/expense-tracker'
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', () => {
     console.log('mongodb error!')
@@ -8,3 +10,4 @@ db.once('open', () => {
     console.log('mongodb connected!')
 })
 module.exports = db
+
