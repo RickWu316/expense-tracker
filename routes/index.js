43a3ac3db4864fdc27b70203d391b310c6a0878c
Rick Wu <rickwu316@gmail.com>
2021-01-07 14:33:25 +0800

    feat: change router

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
 // 引用 Express 與 Express 路由器
 const express = require('express')
 const router = express.Router()
-
-const home = require('./modules/home')
 const records = require('./modules/records')
 
-router.use('/', home)
+router.get('/', (req, res) => { res.redirect("./records") })  //預設records為首頁
 router.use('/records', records)
 
 
--- routes/modules/records.js
+++ routes/modules/records.js
@@ -1,6 +1,46 @@
 const express = require('express')
 const router = express.Router()
 const records = require('../../models/records')
+const Category = require('../../models/category')
+
+
+
+router.get('/', async (req, res) => {
+
+    const categorise = await Category.find().lean()
+    const filterType = req.query.filter
+    const categoryTransIcons = {}
+    const categoryTransCN = {}
+    let filterRecords = {}
+
+    //定義category轉換關係
+    for (category of categorise) {
+        categoryTransIcons[category.category_name] = category.icon
+        categoryTransCN[category.category_name] = category.category_name_CN
+    }
+
+
+    if (filterType) {  //讓空集合有東西
+        filterRecords = records.find({ 'category': filterType })
+    } else {
+        filterRecords = records.find().lean()
+    }
+
+    filterRecords.find() // 取出 Todo model 裡的所有資料
+        .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+        .then(record => {
+            let filterCN = categoryTransCN[filterType]
+            let totalAmount = 0
+            for (const element in record) {
+                record[element].category = categoryTransIcons[record[element].category]
+                totalAmount += record[element].amount
+            }
+            res.render('index', { record, filterCN, totalAmount })
+
+        }) // 將資料傳給 index 樣板
+        .catch(error => console.error(error)) // 錯誤處理
+})
+
 
 router.get('/:id/edit', (req, res) => {
     const id = req.params.id
