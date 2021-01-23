a30e26c2937b21959f0d6816a0c885799aeb644d
Rick Wu <rickwu316@gmail.com>
2021-01-24 02:08:19 +0800

    feat: add flash message

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express()
 const { get } = require('http');
 const PORT = process.env.PORT || 3000
 const routes = require('./routes')// 引用路由器
+const flash = require('connect-flash')
 const usePassport = require('./config/passport')
 
 
@@ -34,11 +35,16 @@ Handlebars.registerHelper('ifEquals', function (arg1, arg2, options) {
 require('./config/mongoose')
 
 usePassport(app)
+app.use(flash())  // 掛載套件
+
 app.use((req, res, next) => {
     res.locals.isAuthenticated = req.isAuthenticated()
     res.locals.user = req.user
+    res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+    res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
     next()
 })
+
 // // 將 request 導入路由器
 app.use(routes)
 
