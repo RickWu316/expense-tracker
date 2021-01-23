3cf642cc25225753802ac23330eba950ec326bf7
Rick Wu <rickwu316@gmail.com>
2021-01-24 02:27:49 +0800

    feat: add bcrypt in seeds

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,6 +1,13 @@
+const bcrypt = require('bcryptjs')
 const db = require('../../config/mongoose')
 const records = require('../records')
+const User = require('../user')
 const Category = require('../category')
+const SEED_USER = {
+    name: 'root',
+    email: 'root@example.com',
+    password: '12345678'
+}
 
 
 db.once('open', async () => {
@@ -14,26 +21,40 @@ db.once('open', async () => {
 
         category.forEach(element => category_name.push(element.category_name))
 
-        for (let i = 0; i < 10; i++) {
-            let categoryRandom = Math.floor(Math.random() * 4)
-            let monthRandom = Math.floor(Math.random() * 12) + 1
-            let dateRandom = Math.floor(Math.random() * 30) + 1
+        bcrypt
+            .genSalt(10)
+            .then(salt => bcrypt.hash(SEED_USER.password, salt))
+            .then(hash => User.create({
+                name: SEED_USER.name,
+                email: SEED_USER.email,
+                password: hash
+            }))
+            .then((user) => {
+                const userId = user._id
+                for (let i = 0; i < 10; i++) {
+                    let categoryRandom = Math.floor(Math.random() * 4)
+                    let monthRandom = Math.floor(Math.random() * 12) + 1
+                    let dateRandom = Math.floor(Math.random() * 30) + 1
 
-            record.push(
-                {
-                    name: 'name-' + i,
-                    category: category_name[categoryRandom],
-                    merchant: '隨便哪裡',
-                    date: `2020-${monthRandom > 9 ? monthRandom : '0' + monthRandom}-${dateRandom > 9 ? dateRandom : '0' + dateRandom}`,
-                    amount: Math.floor(Math.random() * 1000)
-                })
-        }
-        records.create(record)
-            .then(() => {
-                console.log('done')
-                return db.close()
-            }
-            )
+                    record.push(
+                        {
+                            name: 'name-' + i,
+                            category: category_name[categoryRandom],
+                            merchant: '隨便哪裡',
+                            date: `2020-${monthRandom > 9 ? monthRandom : '0' + monthRandom}-${dateRandom > 9 ? dateRandom : '0' + dateRandom}`,
+                            amount: Math.floor(Math.random() * 1000),
+                            userId: userId
+                        })
+
+                }
+                records.create(record)
+                    .then(() => {
+                        console.log('done')
+                        return db.close()
+                    }
+                    )
+            })
+
 
     } catch (error) {
         console.log(error)
